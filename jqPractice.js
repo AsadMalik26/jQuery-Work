b598dc872dc096b90b5c50cac00bb6e779b9d0c9
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-02-11 01:26:40 +0500

    fade effect delete
    Also trying innerHtml

--- jqPractice.js
+++ jqPractice.js
@@ -10,7 +10,10 @@ $(function() {
 
 function handleBtnClick() {
     /*alert("Button Clicked"); // Just for check */
-    var newTodo = $("#newTodo").val(); //read value from input
+    var newTodo = $("#newTodo");
+    console.log(typeof(document.getElementById("newTodo").innerHTML));
+    // document.getElementById("newTodo").innerHTML = "Abcdefghijklmnopqrstuvwxyz";
+    newTodo = newTodo.val(); //read value from input
     if(!newTodo)
     {
         $("#newTodo").addClass("error");
@@ -24,10 +27,12 @@ function handleBtnClick() {
     $("#todos").append("<li>" + newTodo + "</li>"); //append input value in list item
     // $("#todos li").click(removeMe); 1st: read below line, 2nd: we can also handle it in bindings area only by binding just #todo insted of its list items. Now go to bindings area.
     //first see the removeMe() function, now,we are binding this remove event to the new/appended list items.
+    
 }
 
 function removeMe() {
-    $(this).remove();
+    $(this).fadeOut(); //remove with a fade effect
+    // $(this).remove(); //simple remove()
     //this is not working for new appended list items, the reason is that it is not binded to the new/appended list items.
     // so, we have to bind it to the new list items in the handleBtnClick() function
 }
