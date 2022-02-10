3ae5eb9a3d678380d6f1da28365cf34fe8112f3b
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-02-10 23:23:47 +0500

    Add/Delete using jQuery - also a more task
    In previous Practice of add/remove using javascript we binded delete button to the new list items and the current were already with delete button
    
    We did a little bit same in this using jQuery. jQuery helped alot as it saves times and lines of codes and it was easy too.
    We add the data, Okay
    We delete the li node without having a delete button just by picking the id of ul and calling remove() function on the particular node.
    
    There was a problem in new li elemtens as there was missing remove binding, we handled that and did binding to new li elements.
    
    Then we modified as we did a single binding instead of duplicate binding at two places i.e. repeatition of code.
    
    Done.
    
    Task:
    Now focus on how to update/edit the content/element using jQuery

---
+++ jqPractice.js
@@ -0,0 +1,33 @@
+$(function() {
+    // jQuery will execute this function after the page load
+    // So, do all bindings here
+    $("#addBtn").click(handleBtnClick);
+    // $("#todos li").click(removeMe); Now, we are going to simplify on single binding here, which is reducing our duplication too. 
+    $("#todos").on("click", "li", removeMe);
+    // bind #todos, when user click on #todos("consider click on", "li element", "and initialize removeMe function to it. i.e. li element");
+    // now we reduced the duplication, as it was duplicated in handleBtnClick() function too.
+});
+
+function handleBtnClick() {
+    /*alert("Button Clicked"); // Just for check */
+    var newTodo = $("#newTodo").val(); //read value from input
+    if(!newTodo)
+    {
+        $("#newTodo").addClass("error");
+        return;
+    }
+    else
+    {
+        $("#newTodo").removeClass("error");
+    }
+    $("#newTodo").val(""); //reset input to empty
+    $("#todos").append("<li>" + newTodo + "</li>"); //append input value in list item
+    // $("#todos li").click(removeMe); 1st: read below line, 2nd: we can also handle it in bindings area only by binding just #todo insted of its list items. Now go to bindings area.
+    //first see the removeMe() function, now,we are binding this remove event to the new/appended list items.
+}
+
+function removeMe() {
+    $(this).remove();
+    //this is not working for new appended list items, the reason is that it is not binded to the new/appended list items.
+    // so, we have to bind it to the new list items in the handleBtnClick() function
+}
